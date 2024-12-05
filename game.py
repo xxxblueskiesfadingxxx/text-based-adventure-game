3f2a2619ce5d7a82a8fafe9c90f7bb74a19038f3
xxxblueskiesfadingxxx <octobervole09@gmail.com>
2024-12-05 14:10:26 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
-print(hello world!)
+cities = [
+  "los angeles"
+  "las vegas"
+  "austin"
+  "chicago"
+  "atlanta"
+  "philadelphia"
+  "new york city"
+
+print("Hello criminal! You are currently on the run for murder, and you are wanted by the FBI. You will start in Los Angeles and cross 7 cities before you sneak out of the country into a country that won't send you back. I wish you the best of luck!")
